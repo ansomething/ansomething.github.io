00117176df2a13b7d9a7117f9a1267bcc4d83242
andressa <andy.andressa01@gmail.com>
2024-12-03 17:07:16 -0300

    edited

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function sendMail() {
         name : document.getElementById('name').value,
         email : document.getElementById('email').value,
         subject : document.getElementById('subject').value,
-        msg : document.getElementById('message').value,
+        message : document.getElementById('message').value,
     }
 
-    emailjs.send('service_h2dceel', 'template_eryjwrs', params).then(alert('E-mail enviado!'))
+    emailjs.send('service_h2dceel', 'template_2tqmqpw', params).then(alert('E-mail enviado!'))
     
 }
