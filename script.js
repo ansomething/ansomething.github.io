387b0b9a12e78a80a216f87f536dca17b34ead6d
Andressa Vieira <andy.andressa01@gmail.com>
2024-12-03 16:25:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function sendMail() {
         name : document.getElementById('name').value,
         email : document.getElementById('email').value,
         subject : document.getElementById('subject').value,
-        msg : document.getElementById('message').value,
+        message : document.getElementById('message').value,
     }
 
     emailjs.send('service_h2dceel', 'template_eryjwrs', params).then(alert('E-mail enviado!'))
