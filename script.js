d23a0cbc66f71f463310c638c18840878df7dfcd
andressa <andy.andressa01@gmail.com>
2024-12-03 14:15:25 -0300

    03/12v3

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendMail() {
+    let params = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,
+        subject : document.getElementById('subject').value,
+        msg : document.getElementById('msg').value,
+    }
+
+    emailjs.send('service_h2dceel', 'template_eryjwrs', params).then(alert('E-mail enviado!'))
+    
+}
