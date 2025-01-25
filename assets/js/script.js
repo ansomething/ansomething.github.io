6251768c83725cc4a378b78a71fb0528b6e8ba43
andressa <andy.andressa01@gmail.com>
2025-01-24 21:40:38 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,37 @@
-let menuOn = document.getElementById("menuOn");
-let menuOff = document.getElementById("menuOff");
-let sideBar = document.getElementById("sideBar");
 
-function menuNav() {
-    if (menuOn.style.display == "block") {
-        menuOn.style.display = "none";
-        menuOff.style.display = "block";
-        sideBar.style.display = "block";
+
+// let menuOn = document.getElementById("menuOn");
+// let menuOff = document.getElementById("menuOff");
+// let sideBar = document.getElementById("sideBar");
+
+// function menuNav() {
+//     if (menuOn.style.display == "block") {
+//         menuOn.style.display = "none";
+//         menuOff.style.display = "block";
+//         sideBar.style.display = "block";
         
-    } else {
-        menuOn.style.display = "block";
-        menuOff.style.display = "none";
-        sideBar.style.display = "none";
-    }
-}
+//     } else {
+//         menuOn.style.display = "block";
+//         menuOff.style.display = "none";
+//         sideBar.style.display = "none";
+//     }
+// }
 
-let langOn = document.getElementById("langOn");
-let langOff = document.getElementById("langOff");
-let langDrop = document.getElementById("langDrop");
+// let langOn = document.getElementById("langOn");
+// let langOff = document.getElementById("langOff");
+// let langDrop = document.getElementById("langDrop");
 
-function menuLang() {
-    if (langOn.style.display == "block") {
-        langOn.style.display = "none";
-        langOff.style.display = "block";
-        langDrop.style.display = "block";
-    } else {
-        langOn.style.display = "block";
-        langOff.style.display = "none";
-        langDrop.style.display = "none";
-    }
-}
+// function menuLang() {
+//     if (langOn.style.display == "block") {
+//         langOn.style.display = "none";
+//         langOff.style.display = "block";
+//         langDrop.style.display = "block";
+//     } else {
+//         langOn.style.display = "block";
+//         langOff.style.display = "none";
+//         langDrop.style.display = "none";
+//     }
+// }
 
-let slideCopy = document.querySelector('.slide-track__slide').cloneNode(true);
-document.querySelector('.slide-track').appendChild(slideCopy);
+// let slideCopy = document.querySelector('.slide-track__slide').cloneNode(true);
+// document.querySelector('.slide-track').appendChild(slideCopy);
